cf76d5dfdc037b5f6111e6650143f808c745a53a
Junhan0037 <wnsgks0037@naver.com>
2021-04-21 23:08:15 +0900

    feat: 헤더와 드롭다운 메뉴
    - 전역 배지 (GSAP) (2)

--- js/main.js
+++ js/main.js
@@ -18,6 +18,20 @@ searchInputEl.addEventListener('blur', function () {
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', function () {
-    console.log('scroll!');
-});
+// _.throttle(함수, 시간)
+// gsap.to(요소, 지속시간, 옵션)
+window.addEventListener('scroll', _.throttle(function () {
+    if (window.scrollY > 500) {
+        // 배지 숨기기
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        });
+    } else {
+        // 배지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        });
+    }
+}, 300));
