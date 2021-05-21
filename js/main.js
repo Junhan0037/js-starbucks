6e345518543360cc0af1c0ead13d61f0f4def6a9
Junhan0037 <wnsgks0037@naver.com>
2021-05-21 21:55:49 +0900

    feat: 페이지 상단으로 이동
    - ScrollTo

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ searchInputEl.addEventListener('blur', function () {
 
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector("#to-top");
 
 // _.throttle(함수, 시간)
 // gsap.to(요소, 지속시간, 옵션)
@@ -27,15 +28,28 @@ window.addEventListener('scroll', _.throttle(function () {
             opacity: 0,
             display: 'none'
         });
+        // 버튼 보이기
+        gsap.to(toTopEl, .2, {
+            x: 0
+        });
     } else {
         // 배지 보이기
         gsap.to(badgeEl, .6, {
             opacity: 1,
             display: 'block'
         });
+        // 버튼 숨기기
+        gsap.to(toTopEl, .2, {
+            x: 100
+        });
     }
 }, 300));
 
+toTopEl.addEventListener('click', function () {
+    gsap.to(window, .7, {
+        scrollTo: 0
+    });
+});
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function (fadeEl, index) {
